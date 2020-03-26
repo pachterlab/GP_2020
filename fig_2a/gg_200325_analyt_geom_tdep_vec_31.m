94f6edf1bb53e13e61061456f7b9cab6589cd187
gennadygorin <43971338+gennadygorin@users.noreply.github.com>
2020-03-25 21:13:15 -0700

    Add files via upload

--- fig_2a/gg_200325_analyt_geom_tdep_vec_31.m
+++ fig_2a/gg_200325_analyt_geom_tdep_vec_31.m
@@ -1,4 +1,5 @@
-function P=gg_200325_analyt_geom_tdep_vec_31(Kini,b,g,M,N,T,marg,N_approx_taylor,N_approx_laurent)
+function P=gg_200325_analyt_geom_tdep_vec_31(Kini,b,g,M,N,T,...
+    marg,N_approx_taylor,N_approx_laurent)
 if strcmp(marg,'mature')
     M=1;
 elseif strcmp(marg,'nascent')
@@ -19,15 +20,7 @@ v_ = exp(-1i*th2)-1;
 
 % I___ = NaN(M*N,1);
 alpha=((1+sqrt(3))/2)/b;
-% [roots1,xt1,s01]=root_ID(g,TH1,TH2,alpha,I,J);
 [roots,xt,s0]=root_ID(g,TH1,TH2,alpha,I,J);
-% f=find(any(roots-roots1>1e-4,2));
-% length(f)
-% if length(f)>10
-% s01(f(1:10),:)
-% s0(f(1:10),:)
-% end
-% root_viz(b,g,U,V,alpha,M,N,8,8,roots)
 
 num_intervals = sum(roots<T,2)+1;
 interval_ID_taylor = mod(sum(isfinite(roots),2),2)==0;
@@ -80,257 +73,8 @@ INTVAL = INTVAL + ICOMP;
 
 I___=exp(Kini*INTVAL);
 
-% if M>1
-    I___ = reshape(I___,[M,N])';
-    P=real(ifft2(I___))';
-% else
-%     P=real(ifft(I___'));
-% end
-return
-
-function I = numint(b,g,u,v,t1,t2)
-f = @(x) (b*exp(-g*x).*(u+g*v*x))./(1-b*exp(-g*x).*(u+g*v*x));
-I = integral(f,t1,t2);
-% I=exp(KINI*INTVAL);
-return
-
-function root_viz(b,g,U,V,alpha,M,N,Mmax,Nmax,s)
-t=5;
-n_t = 1e4;
-U=reshape(U,[M,N]);
-V=reshape(V,[M,N]);
-s=reshape(s,[M,N,3]);
-
-figure(3)
-clf;
-for i = 1:min(Mmax,M)
-    for j = 1:min(Nmax,N)
-        u = U(i,j);
-        v = V(i,j);
-        figure(3)
-        subplot(Mmax,Nmax,Nmax*(i-1)+j);hold on;
-        t_ =linspace(0,t,n_t);
-        fval = abs(b*(u+g*v*t_).*exp(-g*t_));
-        plot(t_,fval,'k-');hold on;
-        set(gca,'xtick',[],'ytick',[]);
-
-        plot([0 t],alpha*[1 1],'b-');
-
-        yl=ylim;
-        for SIND = 1:3
-            plot(s(i,j,SIND)*[1 1],yl,'r--')
-        end
-    end
-end
-
-return
-
-function integral_viz(b,g,u,v,alpha,i,j,Mmax,Nmax,s)
-t=5;
-if i<=Mmax && j<=Nmax
-    figure(3)
-    subplot(Mmax,Nmax,Nmax*(i-1)+j);hold on;
-    t_ =linspace(0,t);
-    U = abs(b*(u+g*v*t_).*exp(-g*t_));
-    plot(t_,U,'k-');hold on;
-    set(gca,'xtick',[],'ytick',[]);
-    
-    plot([0 t],alpha*[1 1],'b-');
-    
-    yl=ylim;
-    for SIND = 1:3
-        plot(s(SIND)*[1 1],yl,'r--')
-    end
-end
-return
-
-function A = func_val(b,g,u,v,s)
-A=b*exp(-g*s)*(u+g*v*s);
-return
-
-function A = func_abs_val(b,g,u,v,s)
-A=abs(b*exp(-g*s).*(u+g*v*s));
-return
-
-function [s,r,s0]=root_ID_old(g,th1,th2,alpha,i,j)
-n = length(th1);
-
-a2=alpha.^2;
-nr_tol=1e-4;
-bisec_tol = nr_tol;
-
-u2=2*(1-cos(th1));
-v2=2*(1-cos(th2));
-uvvu=2*(1+cos(th2-th1)-cos(th1)-cos(th2));
-
-s=NaN(n,3);
-r=NaN(n,2);
-
-exact=true(n,1);
-
-%%%%%
-% filter for i=j=1
-filt_0 = (i==1 & j==1);
-
-%%%%%
-% filter for u=0
-filt_1 = (i==1 & j>1); %filt_1_ind = find(filt_1); 
-filt_1_rt = filt_1 & (v2/exp(2) > a2);
-s(filt_1_rt,1) = -1/g * lambertw(0,-alpha./(sqrt(v2(filt_1_rt))));
-s(filt_1_rt,2) = -1/g * lambertw(-1,-alpha./(sqrt(v2(filt_1_rt))));
-
-%%%%%
-% filter for v=0
-filt_2 = (j==1 & i>1);
-filt_2_rt = filt_2 & (u2 > a2);
-s(filt_2_rt,1) = -log(alpha./(sqrt(u2(filt_2_rt))))/g;
-
-
-%%%%%
-% define subset that requires numerical root finding
-
-filt_inexact = ~(filt_0 | filt_1 | filt_2);
-exact(filt_inexact) = false;
-inexact_ind = find(filt_inexact);
-
-%%%%%
-% calculate polynomial parameters for subset
-B = g * (v2(filt_inexact) - uvvu(filt_inexact));
-A = -g^2 * v2(filt_inexact);
-C = uvvu(filt_inexact)/2 - u2(filt_inexact);
-discrim = B.^2 - 4.*A.*C;
-val_0 = u2(filt_inexact);
-
-%%%%%
-% filter cases with 1 positive root
-filt_r1 = C>0; filt_r1_ind =find(filt_r1);
-r1 = (-B(filt_r1)-sqrt(discrim(filt_r1)))./(2*A(filt_r1));
-val_r1 = exp(-2*g*r1).*(g^2*v2(inexact_ind(filt_r1)).*r1.^2 ...
-    + g*uvvu(inexact_ind(filt_r1)).*r1+u2(inexact_ind(filt_r1)));
-%intersection right of the root
-filt_r1_s3 = val_r1>a2; %filt_r1_s3_ind = find(filt_r1_s3);
-s(inexact_ind(filt_r1_ind(filt_r1_s3)),3) = r1(filt_r1_s3)-log(a2./val_r1(filt_r1_s3))/(2*g);
-r(inexact_ind(filt_r1_ind(filt_r1_s3)),1) = r1(filt_r1_s3);
-%intersection left of the root
-filt_r1_s1 = filt_r1_s3 & (val_0(filt_r1) < a2); 
-s(inexact_ind(filt_r1_ind(filt_r1_s1)),1) = ...
-    r1(filt_r1_s1) .* (a2-val_0(filt_r1_ind(filt_r1_s1))) ./ ...
-                      (val_r1(filt_r1_s1) - val_0(filt_r1_ind(filt_r1_s1)));
-
-
-%%%%%
-% filter cases with no positive roots
-filt_r0 = ~filt_r1 & ((discrim<0) | (B./A>0)); filt_r0_ind = find(filt_r0);
-filt_r0_s3 = val_0(filt_r0)>a2; 
-s(inexact_ind(filt_r0_ind(filt_r0_s3)),3) = ...
-    -log(a2./val_0(filt_r0_ind(filt_r0_s3)))/(2*g);
-r(inexact_ind(filt_r0_ind(filt_r0_s3)),2) = 0;
-
-
-%%%%%
-% filter cases with two positive roots
-filt_r2 = ~(filt_r1 | filt_r0); filt_r2_ind = find(filt_r2);
-r1 = (-B(filt_r2) + sqrt(discrim(filt_r2))) ./ (2*A(filt_r2));
-r2 = (-B(filt_r2) - sqrt(discrim(filt_r2))) ./ (2*A(filt_r2));
-% if any(isnan(r1)) || any(isnan(r2))
-%     error('')
-% end
-val_r1 = exp(-2*g*r1) .*...
-    (g^2*v2(inexact_ind(filt_r2)) .* r1.^2 ...
-    + g*uvvu(inexact_ind(filt_r2)) .* r1 ...
-    + u2(inexact_ind(filt_r2)));
-val_r2 = exp(-2*g*r2) .*...
-    (g^2*v2(inexact_ind(filt_r2)) .* r2.^2 ...
-    + g*uvvu(inexact_ind(filt_r2)) .* r2 ...
-    + u2(inexact_ind(filt_r2)));
-%intersection left of left root
-filt_r2_s1 = (val_0(filt_r2) > a2) & (val_r1<a2);
-s(inexact_ind(filt_r2_ind(filt_r2_s1)),1) = ...
-    r1(filt_r2_s1) .* (a2 - val_0(filt_r2_ind(filt_r2_s1))) ./ ...
-              (val_r1(filt_r2_s1) - val_0(filt_r2_ind(filt_r2_s1)));
-r(inexact_ind(filt_r2_ind(filt_r2_s1)),1) = r1(filt_r2_s1);
-%intersection right of right root
-filt_r2_s3 = val_r2 > a2;
-s(inexact_ind(filt_r2_ind(filt_r2_s3)),3) = ...
-    r2(filt_r2_s3)-log(a2./val_r2(filt_r2_s3))/(2*g);
-r(inexact_ind(filt_r2_ind(filt_r2_s3)),2) = r2(filt_r2_s3);
-%intersection between roots
-filt_r2_s2 = filt_r2_s3 & (val_r1<a2);
-s(inexact_ind(filt_r2_ind(filt_r2_s2)),2) = ...
-    r1(filt_r2_s2) + (r2(filt_r2_s2)-r1(filt_r2_s2))...
-              .* (a2 - val_r1(filt_r2_s2)) ./ ...
-              (val_r2(filt_r2_s2) - val_r1(filt_r2_s2));
-r(inexact_ind(filt_r2_ind(filt_r2_s2)),1) = r1(filt_r2_s2);
-
-% find(~isnan(s(:,2)))
-% disp('');
-          
-%%%%%
-% save initial guess
-s0 = s(filt_inexact,:);
-if sum(~exact)>0
-    P1 = [g^2*v2(filt_inexact), g*uvvu(filt_inexact), u2(filt_inexact)];
-    P2 = [A,B,C];
-    [s_nr,fail]=nr(s0,P1, P2, g, a2, nr_tol);
-%     fprintf('%i NR searches failed.\n',sum(fail))
-    s(inexact_ind(~fail),:)=s_nr(~fail,:);
-    
-    
-    errf = @(x,P1) (P1(:,1).*x.^2 + P1(:,2).*x + P1(:,3)).*exp(-2*g*x) - a2;
-
-    if sum(fail)>0
-        filt_1 = fail & ~isnan(s0(:,1)); n = sum(filt_1);
-        if n>0
-            s(inexact_ind(filt_1),1) = bisec(zeros(n,1), r(inexact_ind(filt_1),1),...
-                P1(filt_1,:) ,g,a2,bisec_tol);
-            if any(isnan(r(inexact_ind(filt_1),1))) 
-                error('invalid bisection 1!')
-            end
-            if max(errf(s(inexact_ind(filt_1),1),P1(filt_1,:))) > bisec_tol || ...
-                any(~isfinite(errf(s(inexact_ind(filt_1),1),P1(filt_1,:))))
-                error('bisec 1 false positive!');
-            end
-        end
-        
-        filt_2 = fail & ~isnan(s0(:,2));  n=sum(filt_2);
-        if n>0
-            if any(isnan(r(inexact_ind(filt_2),1))) || any(isnan(r(inexact_ind(filt_2),2)))
-                error('invalid bisection 2!')
-            end
-            s(inexact_ind(filt_2),2) = bisec( ...
-                r(inexact_ind(filt_2),1), ...
-                r(inexact_ind(filt_2),2),...
-                P1(filt_2,:) ,g,a2,bisec_tol);
-            if max(errf(s(inexact_ind(filt_2),2),P1(filt_2,:))) > bisec_tol|| ...
-                any(~isfinite(errf(s(inexact_ind(filt_2),2),P1(filt_2,:))))
-                error('bisec 2 false positive!');
-            end
-        end
-        
-        filt_3 = fail & ~isnan(s0(:,3)); n=sum(filt_3);
-        if n>0
-            maxr = max(r(inexact_ind(filt_3),:),[],2);
-            s(inexact_ind(filt_3),3) = bisec( ...
-                maxr, ...
-                maxr+10/g,...
-                P1(filt_3,:),g,a2,bisec_tol);
-            if any(isnan(maxr)) 
-                error('invalid bisection 3!')
-            end
-            if max(errf(s(inexact_ind(filt_3),3),P1(filt_3,:))) > bisec_tol || ...
-                any(~isfinite(errf(s(inexact_ind(filt_3),3),P1(filt_3,:))))
-                error('bisec 2 false positive!');
-            end
-        end
-%     s(inexact_ind(filt_1),1)
-    end
-    if any(any(errf(s(inexact_ind,:),P1)> bisec_tol))  
-        error('false positive!!');
-    end
-
-end
-s(s==0)=NaN;
-s=sort(s,2);
+I___ = reshape(I___,[M,N])';
+P=real(ifft2(I___))';
 return
 
 function [s,r,s0]=root_ID(g,th1,th2,alpha,i,j)
@@ -425,7 +169,7 @@ if sum(~exact)>0
     P1 = [g^2*v2(filt_inexact), g*uvvu(filt_inexact), u2(filt_inexact)];
     P2 = [A,B,C];
     [s_nr,fail]=nr(s0,P1, P2, g, a2, nr_tol);
-    fprintf('%i out of %i NR searches failed.\n',sum(fail),sum(~exact))
+%     fprintf('%i out of %i NR searches failed.\n',sum(fail),sum(~exact))
     s(inexact_ind(~fail),:)=s_nr(~fail,:);
     
     
@@ -507,9 +251,6 @@ end
 % errf(c)
 return
 
-function x=fp(x,b1,b2,func,tol)
-return
-
 function [x,fail]=nr(x,P1,P2,g,a2,tol)
 n=size(x,1);
 x0=x;
@@ -563,20 +304,6 @@ lnw =k.*log(b./k) + ...
         + lnw));
 return
 
-function I = numint_power_series(g,u,v,t1,t2,k)
-k=length(k);
-n=length(u);
-I=NaN(n,k);
-for i = 1:k
-    for j = 1:n
-        f = @(x) (exp(-g*x).*(u(j)+g*v(j)*x)).^i;
-        I(j,i) = integral(f,t1(j),t2(j));
-    end
-end
-
-% I=exp(KINI*INTVAL);
-return
-
 function INTVAL = analytint_taylor(b,g,u,v,I,J,t_1,t_2,N_approx)
 n=length(u);
 XX = NaN(n,N_approx);
@@ -601,8 +328,8 @@ end
 filt_2 = I==1 & J>1; n_filt_2=sum(filt_2);
 if n_filt_2>0
     XX(filt_2,:) = k.*log(v(filt_2)) - log(k*g) + gammaln(k+1) + ...
-            log(gammainc(g*k.*t_2(filt_2),repmat(k+1,[n_filt_2,1]),'upper')-...
-            gammainc(g*k.*t_1(filt_2),repmat(k+1,[n_filt_2,1]),'upper'));
+            log(gammainc(real(g*k.*t_2(filt_2)),repmat(k+1,[n_filt_2,1]),'upper')-...
+            gammainc(real(g*k.*t_1(filt_2)),repmat(k+1,[n_filt_2,1]),'upper'));
 end
 
 %%%%%
@@ -618,15 +345,6 @@ end
 % YY=log(-numint_power_series(g,u,v,t_1,t_2,k));
 INTVAL = sum(-exp(XX+lnw),2);
 INTVAL(filt_0) = 0;
-return
-
-function I = numint_laurent_power_series(g,u,v,t1,t2,k)
-I=NaN(1,k+1);
-for i = 0:k
-    f = @(x) (exp(-g*x).*(u+g*v*x)).^-i;
-    I(i+1) = integral(f,t1,t2);
-end
-
 return
 
 function INTVAL = analytint_laurent(b,g,u,v,I,J,t_1,t_2,N_approx)
